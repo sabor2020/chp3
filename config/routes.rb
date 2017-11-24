4affda8e505a0d2fc38eb66a9ad7c130abe88ea5
Nesar Ahmad <st119798@ait.asia>
2017-11-24 11:43:59 +0700

    After scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "welcome/index", to: "welcome#index"
   get "welcome/about", to: "welcome#about"
